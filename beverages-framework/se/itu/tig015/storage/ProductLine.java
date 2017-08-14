20c164acd2c4c3ebc8da687ff6ebef5227622815
Rikard Fröberg <rikard@morus.se>
2017-08-14 12:27:38 +0200

    Added some javadoc

--- beverages-framework/se/itu/tig015/storage/ProductLine.java
+++ beverages-framework/se/itu/tig015/storage/ProductLine.java
@@ -4,11 +4,24 @@ import java.util.function.Predicate;
 import java.util.List;
 
 import se.itu.tig015.domain.Product;
-  
+
+/**
+ * Represents a ProductLine of beverages. This interface provides methods
+ * for accessing the products in the product line.
+ */
 public interface ProductLine {
 
+  /**
+   * Returns a List&lt;Product&gt; with all products of the product line that satisfies the predicate.
+   * @param predicate A Predicate with the criteria for the products to return
+   * @return A List&lt;Product&gt; with all the Products that match the predicate
+   */
   public List<Product> getProductsFilteredBy(Predicate<Product> predicate);
-  
+
+  /**
+   * Returns a list of all the products in the product line.
+   * @return A List&lt;Product&gt; with all the Products in the product line.
+   */
   public List<Product> getAllProducts();
 
 }
