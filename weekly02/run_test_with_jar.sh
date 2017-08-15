3cbcdad1d6dd366fe5d8ed79d2231ebd2109eab9
Rikard Fröberg <rikard@morus.se>
2017-08-15 17:50:29 +0200

    Added test to make sure xml file is present

--- weekly02/run_test_with_jar.sh
+++ weekly02/run_test_with_jar.sh
@@ -5,4 +5,9 @@ if [[ $OSTYPE == "cygwin" ]] ; then
     PATHSEP=";"
 fi
 
+if [ ! -f "resources/sortiment.xml" ]
+then
+    ./get_latest_file.sh
+fi
+
 java -cp "systemet.jar${PATHSEP}." -Dsortiment-xml-file=resources/sortiment.xml TestWithJar
