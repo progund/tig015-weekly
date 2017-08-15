63566c539fa642610d3c1c0cae6ec800da1d56ab
Rikard Fröberg <rikard@morus.se>
2017-08-15 18:25:05 +0200

    Added some tests for existence of sortiment.xml and resources/

--- weekly02/get_latest_file.sh
+++ weekly02/get_latest_file.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [[ -f "resources/sortiment.xml" ]]
+then
+    rm resources/sortiment.xml
+fi
+
+if [ ! -d "resources" ]
+then
+    mkdir resources
+fi
 wget 'https://www.systembolaget.se/api/assortment/products/xml' -O resources/sortiment.xml
 tidy -indent -utf8 -xml resources/sortiment.xml > resources/formatted.xml
 mv resources/formatted.xml resources/sortiment.xml
