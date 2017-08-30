f73ffc022c141a151e07b9a5249a1a4f107e0844
Rikard Fröberg <rikard@morus.se>
2017-08-30 21:48:54 +0200

    Made sanity a little more verbose

--- weekly02/sanity.sh
+++ weekly02/sanity.sh
@@ -2,7 +2,14 @@
 
 commands="tidy wget java javac zip unzip"
 
+missing=""
 for command in $commands
 do
-    which $command &> /dev/null || echo You must install $command
+    which $command &> /dev/null && echo "$command OK!" || missing="$missing $command"        
 done
+if [ "" = "$missing" ]
+then
+    echo -e "\nYou are all set to go!"
+else
+    echo -e "\nResult:\nYou are missing $missing"
+fi
