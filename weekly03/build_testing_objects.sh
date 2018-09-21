da0ac0c9ea918761b4762f4a66a456a0109b31d2
Henrik Sandklef <hesa@sandklef.com>
2018-09-21 13:42:50 +0200

    Only download if missing file

--- weekly03/build_testing_objects.sh
+++ weekly03/build_testing_objects.sh
@@ -11,8 +11,17 @@ then
     PATHSEP=";"
 fi
 
-./get_latest_file.sh || bailout "Couldn't get xml file"
-./get_latest_jar.sh || bailout "Couldn't get systemet.jar"
+if [ ! -f "systemet.jar" ]
+   ./get_latest_jar.sh || bailout "Couldn't get systemet.jar"
+fi
+   
+if [ ! -f "systemet.jar" ]
+then
+    echo -e "${0}: No jar file found. Fetching...\n"
+    ./get_latest_file.sh || bailout "Couldn't get xml file"
+    echo -e "\n\n${0}: Done fetching jar file. Saved as systemet.jar ."
+    sleep 1
+fi
 
 echo "Compiling TestingObjects.java"
 javac -cp "systemet.jar${PATHSEP}." TestingObjects.java
